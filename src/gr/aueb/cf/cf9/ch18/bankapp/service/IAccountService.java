787fdcff96bb856020157243d3eb844da2cb568b
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-01-07 20:23:49 +0200

    feat: add account service contract

---
+++ src/gr/aueb/cf/cf9/ch18/bankapp/service/IAccountService.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.cf9.ch18.bankapp.service;
+
+import gr.aueb.cf.cf9.ch14.bank_app.InsufficientBalanceException;
+import gr.aueb.cf.cf9.ch18.bankapp.core.exceptions.AccountNotFoundException;
+import gr.aueb.cf.cf9.ch18.bankapp.dto.AccountDepositDTO;
+import gr.aueb.cf.cf9.ch18.bankapp.dto.AccountInsertDTO;
+import gr.aueb.cf.cf9.ch18.bankapp.dto.AccountReadOnlyDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IAccountService {
+    AccountReadOnlyDTO createNewAcount(AccountInsertDTO insertDTO);
+    void deposit(AccountDepositDTO depositDTO) throws AccountNotFoundException;
+    void withdraw(AccountDepositDTO depositDTO) throws InsufficientBalanceException, AccountNotFoundException;
+    BigDecimal getBalance(String iban) throws AccountNotFoundException;
+    List<AccountReadOnlyDTO> getAllAccount();
+}
